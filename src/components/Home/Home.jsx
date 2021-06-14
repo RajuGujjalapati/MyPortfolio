c58737efdca18b72a3856406c2d170a50081a7a7
ems <Password@123>
2021-06-14 15:32:30 +0530

    exact added to routing

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return ( 
+        <div>
+            <h1>Iam The Home page</h1>
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Projects.css';
+
+const Projects = () => {
+    return ( 
+        <div>
+            <h1>Im projects page</h1>
+        </div>
+     );
+}
+ 
+export default Projects;
