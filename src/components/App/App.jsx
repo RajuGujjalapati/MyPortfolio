6d1312067d5922599433d3be2febb811b40041b4
ems <Password@123>
2021-06-16 12:13:50 +0530

    implemented programmatic navigation with home btn

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,8 @@ import Contact from '../Contact/Contact';
 import Home from '../Home/Home';
 import Projects from '../Projects/Projects';
 import Skills from '../Skills/Skills';
+import NavBar from '../NavBar/NavBar'
+import GoHome from '../GoHome/GoHome';
 import './App.css';
 
 
@@ -15,11 +17,13 @@ const App = () => {
         <div>
            <BrowserRouter>
            <div>
+               <NavBar/>
                <Route path="/" exact component={Home}/>
                <Route path="/about" component={About}/>
                <Route path="/projects" component={Projects}/>
                <Route path="/skills" component={Skills}/>
                <Route path="/contact" component={Contact}/>
+                <GoHome/>
 
            </div>
            </BrowserRouter>
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './NavBar.css';
+import { slide as Menu } from 'react-burger-menu';
+import {Link, withRouter} from 'react-router-dom';
+class NavBar extends React.Component{
+    render(){
+      const { location } = this.props;
+      // console.log(location);
+      // This below code helps to hightlight the component which we'r in.
+      const homeClass = location.pathname === '/' ? 'active-item' : '';
+        const aboutClass = location.pathname === '/about' ? 'active-item' : '';
+        const projectsClass =
+            location.pathname === '/projects' ? 'active-item' : '';
+        const skillsClass =
+            location.pathname === '/skills' ? 'active-item' : '';
+        const contactClass =
+            location.pathname === '/contact' ? 'active-item' : '';
+        
+        return (
+            <Menu>
+                {/* We never want to use <a> inside our react router application */}
+              {/* <a id="home" className="menu-item" href="/">Home</a>
+              <a id="about" className="menu-item" href="/about">About</a>
+              <a id="contact" className="menu-item" href="/contact">Contact</a>
+              <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a> */}
+              <Link to="/" className={`menu-item ${homeClass}`}>
+                Home
+              </Link>
+              <Link to="/about" className={`menu-item ${aboutClass}`}>
+                About
+                </Link>
+              <Link to="/skills" className={`menu-item ${skillsClass}`}>
+                Skills
+                </Link>
+                <Link to="/projects" className={`menu-item ${projectsClass}`}>
+                Projects
+                </Link>
+              <Link to="/contact" className={`menu-item ${contactClass}`}>
+                Contact
+                </Link>
+              
+            </Menu>
+          );
+      
+    }
+  }
+  
+export default withRouter(NavBar);
