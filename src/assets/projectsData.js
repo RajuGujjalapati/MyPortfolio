427395982c4fa668ff79e05040c4d9b5de873e0e
ems <Password@123>
2021-06-23 16:49:22 +0530

    added deleted files

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -1,60 +1,61 @@
 export const projectsData = [
     {
-        projectName: 'Name It!',
-        projectDescription:
-            'Do you find it difficult to name your projects or ventures? Fear not! Check out this cool name suggestion application which will help you find the best name for your next project, venture, or startup. Keep building!',
-        imageUrl: 'https://user-images.githubusercontent.com/57700376/123074541-052b2180-d435-11eb-85fd-c2a64f99337c.png',
-        codeUrl:
-            'https://github.com/RajuGujjalapati/nameit.git',
-        projectUrl: 'https://rajugujjalapati.github.io/nameit/',
+    projectName: 'Name It!',
+    projectDescription:
+    'Do you find it difficult to name your projects or ventures? Fear not! Check out this cool name suggestion application which will help you find the best name for your next project, venture, or startup. Keep building!',
+    imageUrl: 'https://user-images.githubusercontent.com/57700376/123074541-052b2180-d435-11eb-85fd-c2a64f99337c.png',
+    codeUrl:
+    'https://github.com/RajuGujjalapati/nameit.git',
+    projectUrl: 'https://rajugujjalapati.github.io/nameit/',
     },
     {
-        projectName: 'FlashType',
-        projectDescription:
-            'Having a good typing speed will always help you as a developer! Here, I present you the typing speed test application, where you can check your typing speed (in wpm) and share the results with your friends too.',
-        imageUrl: 'https://user-images.githubusercontent.com/57700376/123074637-1d02a580-d435-11eb-9e01-9d1442e77b60.png',
-        codeUrl:
-            'https://github.com/RajuGujjalapati/FlashType.git',
-        projectUrl: 'https://rajugujjalapati.github.io/FlashType/',
+    projectName: 'FlashType',
+    projectDescription:
+    'Having a good typing speed will always help you as a developer! Here, I present you the typing speed test application, where you can check your typing speed (in wpm) and share the results with your friends too.',
+    imageUrl: 'https://user-images.githubusercontent.com/57700376/123074637-1d02a580-d435-11eb-9e01-9d1442e77b60.png',
+    codeUrl:
+    'https://github.com/RajuGujjalapati/FlashType.git',
+    projectUrl: 'https://rajugujjalapati.github.io/FlashType/',
     },
     {
-        projectName: 'Portfolio Website',
-        projectDescription:
-            'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. Creating this project helps me to understand more about react. Here i have used Client routing.',
-        imageUrl: 'https://user-images.githubusercontent.com/57700376/122939895-a2cd1500-d391-11eb-8b9b-60457b0e1bc1.png',
-        codeUrl: 'https://github.com/RajuGujjalapati/web_scraping/tree/master/ANDHRA_DATA',
-        projectUrl: 'https://github.com/RajuGujjalapati/web_scraping/tree/master/ANDHRA_DATA',
+    projectName: 'Portfolio Website',
+    projectDescription:
+    'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. Creating this project helps me to understand more about react. Here i have used Client routing.',
+    imageUrl: 'https://user-images.githubusercontent.com/57700376/122939895-a2cd1500-d391-11eb-8b9b-60457b0e1bc1.png',
+    codeUrl: 'https://github.com/RajuGujjalapati/web_scraping/tree/master/ANDHRA_DATA',
+    projectUrl: 'https://github.com/RajuGujjalapati/web_scraping/tree/master/ANDHRA_DATA',
     },
     {
-        projectName: 'Finding Prohibited Properties',
-        projectDescription:
-           'This Project implemented in the aim of a helping to NGO to collect prohibited properties data in andhra pradesh. Here i have used to selenium to scrape and collecting required info and saved as a dataframe for further analysis.',
-        imageUrl: 'https://user-images.githubusercontent.com/57700376/123084774-28f36500-d43f-11eb-9cd6-6caab6894658.jpg',
-        codeUrl: 'https://github.com/RajuGujjalapati/web_scraping/blob/master/web_trending_news.ipynb',
-        projectUrl: 'https://github.com/RajuGujjalapati/web_scraping/blob/master/web_trending_news.ipynb',
+    projectName: 'Finding Prohibited Properties',
+    projectDescription:
+    'This Project implemented in the aim of a helping to NGO to collect prohibited properties data in andhra pradesh. Here i have used to selenium to scrape and collecting required info and saved as a dataframe for further analysis.',
+    imageUrl: 'https://user-images.githubusercontent.com/57700376/123084774-28f36500-d43f-11eb-9cd6-6caab6894658.jpg',
+    codeUrl: 'https://github.com/RajuGujjalapati/web_scraping/blob/master/web_trending_news.ipynb',
+    projectUrl: 'https://github.com/RajuGujjalapati/web_scraping/blob/master/web_trending_news.ipynb',
     },
     {
-        projectName: 'Deduplication',
-        projectDescription:
-           'The Scope of this project is to Find the duplicate users data with their matching percentages. Deduplication helps in filtering Duplicate data where the user created multiple accounts with similar(near) names. This Was an Backend Project, Created multiple api"s and master api to run according to user input.',
-        imageUrl: 'https://user-images.githubusercontent.com/57700376/123075523-eda06880-d435-11eb-962c-a15e7b1fd73c.png',
-        codeUrl: 'https://github.com/RajuGujjalapati/De-duplication',
-        projectUrl: 'https://github.com/RajuGujjalapati/De-duplication',
+    projectName: 'Deduplication',
+    projectDescription:
+    'The Scope of this project is to Find the duplicate users data with their matching percentages. Deduplication helps in filtering Duplicate data where the user created multiple accounts with similar(near) names. This Was an Backend Project, Created multiple api"s and master api to run according to user input.',
+    imageUrl: 'https://user-images.githubusercontent.com/57700376/123075523-eda06880-d435-11eb-962c-a15e7b1fd73c.png',
+    codeUrl: 'https://github.com/RajuGujjalapati/De-duplication',
+    projectUrl: 'https://github.com/RajuGujjalapati/De-duplication',
     },
     {
-        projectName: 'Web Scraping Project(s)',
-        projectDescription:
-           'Web Scraping helps us to get raw data, for further use in analysis,Though scraoing is not legal, But here we"r doing just for educational purposes.This repo has  multiple folders/files. I have developed so many mini projects to scrape the web using pyhton.',
-        imageUrl: 'https://user-images.githubusercontent.com/57700376/123080016-1cb8d900-d43a-11eb-9e7a-55ccc0bdc23a.jpg',
-        codeUrl: 'https://github.com/RajuGujjalapati/web_scraping',
-        projectUrl: 'https://github.com/RajuGujjalapati/web_scraping',
+    projectName: 'Web Scraping Project(s)',
+    projectDescription:
+    'Web Scraping helps us to get raw data, for further use in analysis,Though scraoing is not legal, But here we"r doing just for educational purposes.This repo has multiple folders/files. I have developed so many mini projects to scrape the web using pyhton.',
+    imageUrl: 'https://user-images.githubusercontent.com/57700376/123080016-1cb8d900-d43a-11eb-9e7a-55ccc0bdc23a.jpg',
+    codeUrl: 'https://github.com/RajuGujjalapati/web_scraping',
+    projectUrl: 'https://github.com/RajuGujjalapati/web_scraping',
     },
     {
-        projectName: 'Sentiment Analysis',
-        projectDescription:
-           'This Project implemented in the aim of finding how the news websites are writing their articles in COVID-19 period, Here I have used three famous news websites [CNN, NBC, CNBC]. After collecting the news headlines i did a sentiment analysis and find out the [Postivity, Negativity Rate] on their articles.',
-        imageUrl: 'https://user-images.githubusercontent.com/57700376/123078599-c1d2b200-d438-11eb-81e9-43f4b58dffc6.jpg',
-        codeUrl: 'https://github.com/RajuGujjalapati/web_scraping/blob/master/web_trending_news.ipynb',
-        projectUrl: 'https://github.com/RajuGujjalapati/web_scraping/blob/master/web_trending_news.ipynb',
+    projectName: 'Sentiment Analysis',
+    projectDescription:
+    'This Project implemented in the aim of finding how the news websites are writing their articles in COVID-19 period, Here I have used three famous news websites [CNN, NBC, CNBC]. After collecting the news headlines i did a sentiment analysis and find out the [Postivity, Negativity Rate] on their articles.',
+    imageUrl: 'https://user-images.githubusercontent.com/57700376/123078599-c1d2b200-d438-11eb-81e9-43f4b58dffc6.jpg',
+    codeUrl: 'https://github.com/RajuGujjalapati/web_scraping/blob/master/web_trending_news.ipynb',
+    projectUrl: 'https://github.com/RajuGujjalapati/web_scraping/blob/master/web_trending_news.ipynb',
     },
-];
+    ];
+    
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,49 +7,48 @@ import Footer from '../Footer/Footer';
 import Typewriter from 'typewriter-effect';
 
 const About = () => {
-    return (
-        <div className="section-container">
-            <Header heading="About Me."
-                details="Programmer | Loves Competetive Programming which i never did."
-            />
-            <div className='about-main'>
-                <div className='about-main-left'>
-                    {/* Sub section 1 */}
-                    <b><i className='about-sub-head'>Howdy! A bit of me</i></b>
-                    <p className='about-details'>
-                        I am a Software Engineer who enjoys solving complex problems.<br />
-                        I have a keen interest in Competitive Programming using Data Structure and Algorithms.<br /><br /><br />
-                        <Typewriter
-                            options={{
-                                strings: "Curently i'm having 1+ year(s) of development experience in software industry.<br> <br> <h1> #OpenToWork</h1>. ",
-                                autoStart: true,
-                                loop: true,
-                            }}
-                        />
-                    </p>
-                </div>
+return (
+<div className="section-container">
+<Header heading="About Me."
+details="Programmer | Loves Competetive Programming which i never did."
+/>
+<div className='about-main'>
+<div className='about-main-left'>
+{/* Sub section 1 */}
+<b><i className='about-sub-head'>Howdy! A bit of me</i></b>
+<p className='about-details'>
+I am a Software Engineer who enjoys solving complex problems.<br />
+I have a keen interest in Competitive Programming using Data Structure and Algorithms.<br /><br /><br />
+<Typewriter
+options={{
+strings: "Curently i'm having 1+ year(s) of development experience in software industry.<br> <br> <h1> #OpenToWork</h1>. ",
+autoStart: true,
+loop: true,
+}}
+/>
+</p>
+</div>
 
-                <div className='about-main-right'>
-                    <img
-                        src={aboutAnime}
-                        alt='animation'
-                        className='about-anime'
-                    />
-                </div>
-            </div>
+<div className='about-main-right'>
+<img
+src={aboutAnime}
+alt='animation'
+className='about-anime'
+/>
+</div>
+</div>
 
-            <Footer
-                phrase='Check out my '
-                link='Skills!'
-                toAdress='/skills'
-            />
-            {/* Vector Frame! */}
-            <div className='vector-frame'>
-                <img src={aboutVector} className='about-vector' alt='about' />
-            </div>
-        </div>
-    );
+<Footer
+phrase='Check out my '
+link='Skills!'
+toAdress='/skills'
+/>
+{/* Vector Frame! */}
+<div className='vector-frame'>
+<img src={aboutVector} className='about-vector' alt='about' />
+</div>
+</div>
+);
 };
-
 
 export default About;
