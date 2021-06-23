60cf5356162e2126e4f951a5ed09b7fa4437688a
RAJU <57700376+RajuGujjalapati@users.noreply.github.com>
2021-06-23 17:40:57 +0530

    Add files via upload

---
+++ src/components/GoHome/GoHome.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import homeicon from '../../assets/home_white.png';
+import blackHomeIcon from '../../assets/home_black.png';
+import './GoHome.css';
+// Programmatic navigation
+class GoHome extends Component{
+    navigationToHome = () =>{
+        const { history } = this.props;
+        history.push('/')
+    }
+    render(){
+        const {location} = this.props;
+        const whiteBtn = location.pathname === "/";
+
+        return(
+            <button onClick={this.navigationToHome} className={`go-home-btn ${whiteBtn ? `white-bkg` : `black-bkg gradient-bkg `}`}>
+                <img className="home-icon"
+                src={ whiteBtn ? blackHomeIcon : homeicon } 
+                alt="home-icon" />
+            </button>
+        );
+        
+    }
+}
+
+export default withRouter(GoHome);
